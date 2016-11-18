f8225942ba6598a80b6fe4313b603ae048e9c917
kumazawakento <kento.kumazawa@gmail.com>
2016-11-18 22:46:01 +0900

    fragmentAdapterTest

--- app/src/main/java/com/example/kumazawakento/digitalpamphlet/MainActivity.java
+++ app/src/main/java/com/example/kumazawakento/digitalpamphlet/MainActivity.java
@@ -19,8 +19,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //initTabs();
-
         mAdapter = new MainFragmentPagerAdapter(getSupportFragmentManager());
 
         mPager = (ViewPager)findViewById(R.id.pager);
@@ -40,47 +38,6 @@ public class MainActivity extends AppCompatActivity {
         indicator.setSelectedColor(0xFF000000);
         indicator.setSelectedBold(true);
 
-    }
-
-    protected void initTabs() {
-        try {
-            TabHost tabHost = (TabHost) findViewById(R.id.tabHost);
-            tabHost.setup();
-            TabHost.TabSpec spec;
-
-            // Tab1
-            spec = tabHost.newTabSpec("Tab1")
-                    .setIndicator("Home", ContextCompat.getDrawable(this, R.drawable.gidai))
-                    .setContent(R.id.linearLayout);
-            tabHost.addTab(spec);
-
-            // Tab2
-            spec = tabHost.newTabSpec("Tab2")
-                    .setIndicator("Time Table", ContextCompat.getDrawable(this, R.drawable.gidai))
-                    .setContent(R.id.linearLayout2);
-            tabHost.addTab(spec);
-
-            // Tab3
-            spec = tabHost.newTabSpec("Tab3")
-                    .setIndicator("Food", ContextCompat.getDrawable(this, R.drawable.gidai))
-                    .setContent(R.id.linearLayout3);
-            tabHost.addTab(spec);
-
-            // Tab4
-            spec = tabHost.newTabSpec("Tab4")
-                    .setIndicator("Map", ContextCompat.getDrawable(this, R.drawable.gidai))
-                    .setContent(R.id.linearLayout4);
-            tabHost.addTab(spec);
-
-
-            tabHost.setCurrentTab(0);
-
-
-        } catch (IllegalArgumentException e) {
-            e.printStackTrace();
-        } catch (RuntimeException e) {
-            e.printStackTrace();
-        }
     }
 
 }
